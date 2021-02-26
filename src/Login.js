040d539e5db0cf3b9051962e88d9bb1b77c411b1
Kozhenin Nick <kozhenin.nick@gmail.com>
2021-02-26 14:14:11 +0300

    Переписал форму логина

--- src/Login.js
+++ src/Login.js
@@ -6,58 +6,47 @@ import {
 import { useFormik } from 'formik';
 import axios from 'axios';
 import AppState from '../App';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 
-function Login({ navigation }) {
-  if (AppState.token != null) {
-    alert('Вы уже вошли');
-    navigation.navigate('Home');
+class Login extends React.Component {
+  render() {
+    if (AppState.token != null) {
+      alert('Вы уже вошли');
+      this.props.navigation.navigate('Home');
+    }
+    return (
+      <View>
+        <Text>
+          <Formik
+          initialValues={{
+            login: "",
+            password: "",
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            axios.post('http://studentapi.myknitu.ru/auth/', values).then(response => {
+              if (response.data == 'error') {
+                alert('Проблема с входом в систему');
+              } else {
+                alert('Вы успешно авторизовались');
+                AppState.token = response.data.token;
+                this.props.navigation.navigate('About');
+              }
+            })
+          }}
+          >
+            {({ handleChange, handleBlur, handleSubmit, values }) => (
+            <View>
+              <Field type="text" name="login" placeholder="Логин" />
+              <Field type="text" name="password" placeholder="Пароль" />
+              <Button onPress={handleSubmit} title="Submit" />
+            </View>
+            )}
+          </Formik>
+          </Text>
+      </View>
+    );
   }
-
-  const formik = useFormik({
-    initialValues: {
-      login: '',
-      password: ''
-    },
-    onSubmit: values => {
-      axios.post('http://studentapi.myknitu.ru/auth/', values).then(response => {
-        if (response.data == 'error') {
-          alert('Проблема с входом в систему');
-        } else {
-          alert('Вы успешно авторизовались');
-          AppState.token = response.data.token;
-          navigation.navigate('About');
-        }
-      })
-    },
-  });
-  return (
-    <View>
-      <Text>
-        <form onSubmit={formik.handleSubmit}>
-          <label htmlFor="login">Логин</label>
-          <input
-            style={{ margin: 10, padding: 10 }}
-            id="login"
-            name="login"
-            type="text"
-            onChange={formik.handleChange}
-            value={formik.values.login}
-          />
-          <label htmlFor="password">Пароль</label>
-          <input
-            style={{ margin: 10, padding: 10 }}
-            id="password"
-            name="password"
-            type="password"
-            onChange={formik.handleChange}
-            value={formik.values.password}
-          />
-          <button type="submit">Войти</button>
-        </form>
-      </Text>
-    </View>
-  );
 }
 
 export default Login;
